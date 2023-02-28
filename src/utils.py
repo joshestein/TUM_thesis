275aa876a8eb775da7970db208f90762caac09a4
Josh <josh.e.stein@gmail.com>
2023-02-28 17:24:51 +0100

    Make dirs if not existing

--- src/utils.py
+++ src/utils.py
@@ -7,4 +7,8 @@ def setup_dirs(root_dir=Path(os.getcwd())):
     log_dir = root_dir / "logs"
     out_dir = root_dir / "out"
 
+    os.makedirs(data_dir, exist_ok=True)
+    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(out_dir, exist_ok=True)
+
     return data_dir, log_dir, out_dir
