86e018fdfeb018749c0c3d3f90bcd22c1ec66106
Josh <josh.e.stein@gmail.com>
2023-02-28 18:02:01 +0100

    Formatting

--- src/datasets/acdc_dataset.py
+++ src/datasets/acdc_dataset.py
@@ -9,12 +9,8 @@ from torch.utils.data import Dataset
 class ACDCDataset(Dataset):
     def __init__(self, data_dir: str | Path, train=True, transform=None):
         self.data_dir = Path(data_dir)
-        self.data_dir = (
-            self.data_dir / "training" if train else self.data_dir / "testing"
-        )
-        self.patients = sorted(
-            [f.path for f in os.scandir(self.data_dir) if f.is_dir()]
-        )
+        self.data_dir = self.data_dir / "training" if train else self.data_dir / "testing"
+        self.patients = sorted([f.path for f in os.scandir(self.data_dir) if f.is_dir()])
         self.transform = transform
 
     def __len__(self):
@@ -28,18 +24,10 @@ class ACDCDataset(Dataset):
         end_systole = int(config[1, 1])
 
         patient = patient_dir.name
-        end_diastole_image = nib.load(
-            patient_dir / f"{patient}_frame{end_diastole:02d}.nii.gz"
-        )
-        end_diastole_label = nib.load(
-            patient_dir / f"{patient}_frame{end_diastole:02d}_gt.nii.gz"
-        )
-        end_systole_image = nib.load(
-            patient_dir / f"{patient}_frame{end_systole:02d}.nii.gz"
-        )
-        end_systole_label = nib.load(
-            patient_dir / f"{patient}_frame{end_systole:02d}_gt.nii.gz"
-        )
+        end_diastole_image = nib.load(patient_dir / f"{patient}_frame{end_diastole:02d}.nii.gz")
+        end_diastole_label = nib.load(patient_dir / f"{patient}_frame{end_diastole:02d}_gt.nii.gz")
+        end_systole_image = nib.load(patient_dir / f"{patient}_frame{end_systole:02d}.nii.gz")
+        end_systole_label = nib.load(patient_dir / f"{patient}_frame{end_systole:02d}_gt.nii.gz")
 
         end_diastole_image = end_diastole_image.get_fdata(dtype=np.float32)
         end_diastole_label = end_diastole_label.get_fdata(dtype=np.float32)
