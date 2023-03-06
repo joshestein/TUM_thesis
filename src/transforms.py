4ebf57c5cc4990b8e0949f7d432a927682cac1e0
Josh <josh.e.stein@gmail.com>
2023-03-06 10:33:03 +0100

    Add augmentations

--- src/transforms.py
+++ src/transforms.py
@@ -1,4 +1,16 @@
-from monai.transforms import Compose, EnsureChannelFirstd, Orientationd, DivisiblePadd, NormalizeIntensityd, ToTensord
+from monai.transforms import (
+    Compose,
+    EnsureChannelFirstd,
+    Orientationd,
+    DivisiblePadd,
+    NormalizeIntensityd,
+    ToTensord,
+    RandAdjustContrastd,
+    RandRotated,
+    RandZoomd,
+    RandFlipd,
+)
+from monai.utils import InterpolateMode
 
 
 def get_transforms(image_key: str, label_key: str):
@@ -8,6 +20,14 @@ def get_transforms(image_key: str, label_key: str):
             Orientationd(keys=[image_key, label_key], axcodes="RAS"),
             DivisiblePadd(keys=[image_key, label_key], k=16, mode="reflect"),
             NormalizeIntensityd(keys=[image_key], channel_wise=True),
+            RandRotated(keys=[image_key, label_key], range_x=0.52, range_y=0.52, range_z=0.52, prob=0.5),  # 30 degrees
+            RandAdjustContrastd(keys=[image_key], gamma=(0.7, 1.5), prob=0.5),
+            RandFlipd(keys=[image_key, label_key], spatial_axis=0, prob=0.5),
+            RandFlipd(keys=[image_key, label_key], spatial_axis=1, prob=0.5),
+            RandFlipd(keys=[image_key, label_key], spatial_axis=2, prob=0.5),
+            RandZoomd(
+                keys=[image_key, label_key], min_zoom=0.85, max_zoom=1.25, mode=InterpolateMode.NEAREST, prob=0.5
+            ),
             ToTensord(keys=[image_key, label_key]),
         ]
     )
