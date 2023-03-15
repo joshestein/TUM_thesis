58a4dd8e5451b4aa22fbb27e283bc3be5890f799
Josh <josh.e.stein@gmail.com>
2023-03-15 10:08:00 +0100

    Intensity normalization always at end

--- src/transforms.py
+++ src/transforms.py
@@ -19,7 +19,6 @@ def get_transforms(image_key: str, label_key: str):
             EnsureChannelFirstd(keys=[image_key, label_key], channel_dim="no_channel"),
             Orientationd(keys=[image_key, label_key], axcodes="RAS"),
             DivisiblePadd(keys=[image_key, label_key], k=16, mode="reflect"),
-            NormalizeIntensityd(keys=[image_key], channel_wise=True),
             RandRotated(keys=[image_key, label_key], range_x=0.52, range_y=0.52, range_z=0.52, prob=0.5),  # 30 degrees
             RandAdjustContrastd(keys=[image_key], gamma=(0.7, 1.5), prob=0.5),
             RandFlipd(keys=[image_key, label_key], spatial_axis=0, prob=0.5),
@@ -28,6 +27,7 @@ def get_transforms(image_key: str, label_key: str):
             RandZoomd(
                 keys=[image_key, label_key], min_zoom=0.85, max_zoom=1.25, mode=InterpolateMode.NEAREST, prob=0.5
             ),
+            NormalizeIntensityd(keys=[image_key], channel_wise=True),
             ToTensord(keys=[image_key, label_key]),
         ]
     )
