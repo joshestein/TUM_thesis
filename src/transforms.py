00574c4afa86683cc03beed95736e575114009ac
Josh <josh.e.stein@gmail.com>
2023-03-22 09:37:50 +0200

    Transforms take lists of keys

--- src/transforms.py
+++ src/transforms.py
@@ -13,17 +13,28 @@ from monai.transforms import (
 from monai.utils import InterpolateMode
 
 
-def get_transforms(image_key: str, label_key: str):
+def get_transforms(
+    image_keys: list[str] = None,
+    label_keys: list[str] = None,
+):
+    if image_keys is None:
+        image_keys = ["end_diastole", "end_systole"]
+
+    if label_keys is None:
+        label_keys = ["end_diastole_label", "end_systole_label"]
+
     train_transforms = Compose(
         [
-            EnsureChannelFirstd(keys=[image_key, label_key], channel_dim="no_channel"),
-            Orientationd(keys=[image_key, label_key], axcodes="RAS"),
-            DivisiblePadd(keys=[image_key, label_key], k=16, mode="reflect"),
-            RandRotated(keys=[image_key, label_key], range_x=0.52, range_y=0.52, range_z=0.52, prob=0.5),  # 30 degrees
-            RandAdjustContrastd(keys=[image_key], gamma=(0.7, 1.5), prob=0.5),
-            RandFlipd(keys=[image_key, label_key], spatial_axis=[0, 1, 2], prob=0.5),
+            EnsureChannelFirstd(keys=[*image_keys, *label_keys], channel_dim="no_channel"),
+            Orientationd(keys=[*image_keys, *label_keys], axcodes="RAS"),
+            DivisiblePadd(keys=[*image_keys, *label_keys], k=16, mode="reflect"),
+            RandRotated(
+                keys=[*image_keys, *label_keys], range_x=0.52, range_y=0.52, range_z=0.52, prob=0.5
+            ),  # 30 degrees
+            RandAdjustContrastd(keys=[*image_keys], gamma=(0.7, 1.5), prob=0.5),
+            RandFlipd(keys=[*image_keys, *label_keys], spatial_axis=[0, 1, 2], prob=0.5),
             RandZoomd(
-                keys=[image_key, label_key], min_zoom=0.85, max_zoom=1.25, mode=InterpolateMode.NEAREST, prob=0.5
+                keys=[*image_keys, *label_keys], min_zoom=0.85, max_zoom=1.25, mode=InterpolateMode.NEAREST, prob=0.5
             ),
             NormalizeIntensityd(keys=[image_key], channel_wise=True),
             ToTensord(keys=[image_key, label_key]),
