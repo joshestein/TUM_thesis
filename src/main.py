1762768cd75c311a736096d35a87dff23f3faf4d
Josh <josh.e.stein@gmail.com>
2023-04-16 13:44:00 +0200

    Double loop for limited data and limited slices

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import tomllib
 from pathlib import Path
@@ -15,11 +16,12 @@ from src.metrics import METRICS
 from src.train import train
 from src.utils import find_optimal_learning_rate, get_train_dataloaders, setup_dirs
 from src.transforms.transforms import get_transforms
+from src.utils import get_train_dataloaders, setup_dirs
 
 
 def main():
     root_dir = Path(os.getcwd())
-    data_dir, log_dir, out_dir = setup_dirs(root_dir)
+    data_dir, log_dir, root_out_dir = setup_dirs(root_dir)
     data_dir = data_dir / "ACDC" / "database"
 
     with open(root_dir / "config.toml", "rb") as file:
@@ -36,33 +38,7 @@ def main():
 
     set_determinism(seed=config["hyperparameters"]["seed"])
 
-    train_transforms = get_transforms(augment)
-    train_data = ACDCDataset(
-        data_dir=data_dir,
-        train=True,
-        transform=train_transforms,
-        percentage_data=percentage_data,
-    )
-
-    train_loader, val_loader = get_train_dataloaders(
-        dataset=train_data,
-        batch_size=batch_size,
-        validation_split=validation_split,
-    )
-
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model = UNet(
-        spatial_dims=3,
-        in_channels=1,
-        out_channels=4,
-        # channels=(26, 52, 104, 208, 416),
-        channels=(16, 32, 64, 128, 256),
-        strides=(2, 2, 2, 2),
-        norm=Norm.INSTANCE,
-        # num_res_units=4,
-        # dropout=0.5,
-    ).to(device)
-
     loss_function = DiceLoss(to_onehot_y=True, softmax=True)
     # TODO: weight decay check
     optimizer = torch.optim.Adam(model.parameters())
@@ -82,24 +58,71 @@ def main():
 
     config["hyperparameters"]["optimal_learning_rate"] = optimal_learning_rate
 
-    wandb.init(project="acdc-3D-UNet-baseline-restart", config=config["hyperparameters"], dir=log_dir, reinit=True)
-    wandb.config.dataset = "ACDC"
-    wandb.config.architecture = "UNet"
-
     val_interval = 5
 
-    epoch_loss_values, metric_values = train(
-        model=model,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        loss_function=loss_function,
-        optimizer=optimizer,
-        val_interval=val_interval,
-        epochs=epochs,
-        metrics=METRICS,
-        device=device,
-        out_dir=out_dir,
-    )
+    for percentage_data in [1.0, 0.8, 0.6, 0.4, 0.2, 0.1, 0.05]:
+        config["hyperparameters"]["percentage_data"] = percentage_data
+
+        for percentage_slices in [0.8, 0.6, 0.4, 0.2, 0.1, 0.05]:
+            config["hyperparameters"]["percentage_slices"] = percentage_slices
+
+            train_transforms = get_transforms(augment, percentage_slices=percentage_slices)
+            train_data = ACDCDataset(
+                data_dir=data_dir,
+                train=True,
+                transform=train_transforms,
+                percentage_data=percentage_data,
+            )
+
+            train_loader, val_loader = get_train_dataloaders(
+                dataset=train_data,
+                batch_size=batch_size,
+                validation_split=validation_split,
+            )
+
+            model = UNet(
+                spatial_dims=3,
+                in_channels=1,
+                out_channels=4,
+                # channels=(26, 52, 104, 208, 416),
+                channels=(16, 32, 64, 128, 256),
+                strides=(2, 2, 2, 2),
+                norm=Norm.INSTANCE,
+                # num_res_units=4,
+                # dropout=0.5,
+            ).to(device)
+
+            optimizer = torch.optim.Adam(model.parameters(), lr=optimal_learning_rate)
+
+            wandb.init(
+                project="acdc-3D-UNet-baseline-restart",
+                config=config["hyperparameters"],
+                dir=log_dir,
+                tags=("limited_data", "limited_slices"),
+                reinit=True,
+            )
+            wandb.config.dataset = "ACDC"
+            wandb.config.architecture = "UNet"
+
+            out_dir = root_out_dir / f"percentage_data_{percentage_slices}" / f"percentage_slices_{percentage_slices}"
+            os.makedirs(out_dir, exist_ok=True)
+
+            epoch_loss_values, metric_values = train(
+                model=model,
+                train_loader=train_loader,
+                val_loader=val_loader,
+                loss_function=loss_function,
+                optimizer=optimizer,
+                val_interval=val_interval,
+                epochs=epochs,
+                metrics=METRICS,
+                device=device,
+                out_dir=out_dir,
+            )
+
+            wandb.finish()
+            torch.cuda.empty_cache()
+            gc.collect()
 
 
 if __name__ == "__main__":
