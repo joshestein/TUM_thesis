e9fd3f2539fc5505b05e1d98d9ab3dab03c8fec8
Josh <josh.e.stein@gmail.com>
2023-05-21 20:33:13 +0530

    Choose sample slices from given area

--- src/transforms/remove_slices.py
+++ src/transforms/remove_slices.py
@@ -1,3 +1,4 @@
+import math
 from typing import Hashable, Mapping
 
 import torch
@@ -13,6 +14,8 @@ class RemoveSlicesd(MapTransform):
     :param random_slices: Whether to remove random slices or slices from the center of the volume. Defaults to `True`.
     :param maintain_shape: Whether to maintain the shape of the data. If `True`, the removed slices will be filled with
             zeros. Defaults to `True`.
+    :param sample_areas: Which area(s) of the heart to sample from. Can be one or more of 'apex', 'mid', 'base'.
+        Defaults to ('apex', 'mid', 'base').
     :param allow_missing_keys: Allow missing keys. If `False`, an exception will be raised if a key is missing. Defaults
             to `True`.
     """
@@ -23,6 +26,7 @@ class RemoveSlicesd(MapTransform):
         percentage_slices: float,
         random_slices=True,
         maintain_shape=True,
+        sample_areas: list[str] = ("apex", "mid", "base"),
         allow_missing_keys=True,
     ):
         super().__init__(keys, allow_missing_keys=allow_missing_keys)
@@ -30,6 +34,10 @@ class RemoveSlicesd(MapTransform):
         self.percentage_slices = percentage_slices
         self.random_slices = random_slices
         self.maintain_shape = maintain_shape
+
+        for area in sample_areas:
+            assert area in ("apex", "mid", "base"), f"Invalid area {area}. Must be one of 'apex', 'mid', 'base'."
+        self.sample_areas = sample_areas
 
     def __call__(self, data: Mapping[Hashable, torch.Tensor]) -> dict[Hashable, torch.Tensor]:
         d = dict(data)
@@ -50,6 +58,16 @@ class RemoveSlicesd(MapTransform):
 
     def _get_indexes_to_remove(self, data: torch.Tensor):
         slices = data.shape[-1]
+        area_slices = {
+            "base": range(0, int(math.ceil(slices / 3))),
+            "mid": range(int(math.ceil(slices / 3)), int(math.ceil(2 * slices / 3))),
+            "apex": range(int(math.ceil(2 * slices / 3)), slices),
+        }
+
+        # TODO: should we ignore first and last n slices if they are filled with constant zeros? After we remove slices,
+        # will always pad again with zeros to ensure divisibility by 16.
+        indexes_to_sample_from = sorted([index for area in self.sample_areas for index in area_slices[area]])
+        slices = len(indexes_to_sample_from)
         # We subtract from one to ensure this is the amount we keep.
         # For example, if 0.8, we want to keep 80% of the data, so we get indexes corresponding to the other 20%.
         num_slices = int(slices * (1 - self.percentage_slices))
