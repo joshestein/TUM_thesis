b33844725f94b32c1d8049d07e7a49833995324b
Josh <josh.e.stein@gmail.com>
2023-06-21 10:56:44 +0100

    Alphabetical metrics

--- src/metrics.py
+++ src/metrics.py
@@ -3,8 +3,8 @@ from monai.metrics import DiceMetric, HausdorffDistanceMetric
 METRICS = {
     "dice": DiceMetric(include_background=False, reduction="mean"),
     "dice_with_background": DiceMetric(include_background=True, reduction="mean"),
-    "hausdorff": HausdorffDistanceMetric(include_background=False, reduction="mean"),
     "dice_per_label": DiceMetric(include_background=False, reduction="mean_batch"),
     "dice_per_label_with_background": DiceMetric(include_background=True, reduction="mean_batch"),
+    "hausdorff": HausdorffDistanceMetric(include_background=False, reduction="mean"),
     "hausdorff_per_label": HausdorffDistanceMetric(include_background=False, reduction="mean_batch"),
 }
