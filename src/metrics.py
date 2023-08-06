f835eb877dab838ff2ece5a95ac064c048babc2f
Josh <josh.e.stein@gmail.com>
2023-08-06 16:47:07 +0200

    Set num classes for Dice Metric

--- src/metrics.py
+++ src/metrics.py
@@ -1,10 +1,10 @@
 from monai.metrics import DiceMetric, HausdorffDistanceMetric
 
 METRICS = {
-    "dice": DiceMetric(include_background=False, reduction="mean"),
-    "dice_with_background": DiceMetric(include_background=True, reduction="mean"),
-    "dice_per_label": DiceMetric(include_background=False, reduction="mean_batch"),
-    "dice_per_label_with_background": DiceMetric(include_background=True, reduction="mean_batch"),
+    "dice": DiceMetric(include_background=False, reduction="mean", num_classes=4),
+    "dice_with_background": DiceMetric(include_background=True, reduction="mean", num_classes=4),
+    "dice_per_label": DiceMetric(include_background=False, reduction="mean_batch", num_classes=4),
+    "dice_per_label_with_background": DiceMetric(include_background=True, reduction="mean_batch", num_classes=4),
     "hausdorff": HausdorffDistanceMetric(include_background=False, reduction="mean"),
     "hausdorff_per_label": HausdorffDistanceMetric(include_background=False, reduction="mean_batch"),
 }
