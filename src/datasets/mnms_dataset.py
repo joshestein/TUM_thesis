df7b46af488e44b9704551ba3a05c84eb1e05a03
Josh <josh.e.stein@gmail.com>
2023-09-17 15:41:16 +0200

    Update MNMs dataloader to handle separate frames

--- src/datasets/mnms_dataset.py
+++ src/datasets/mnms_dataset.py
@@ -23,14 +23,20 @@ class MNMsDataset(Dataset):
         :param shuffle: Whether to shuffle the dataset
         """
         self.data_dir = data_dir
-        self.patients = sorted([Path(f.path) for f in os.scandir(self.data_dir) if f.is_dir()])
+        patient_dirs = sorted([Path(f.path) for f in os.scandir(self.data_dir) if f.is_dir()])
         if shuffle:
-            np.random.shuffle(self.patients)
+            np.random.shuffle(patient_dirs)
+
+        self.patients, self.labels = [], []
+        for patient in patient_dirs:
+            self.patients.extend(sorted(patient.glob("*[0-9].nii.gz")))
+            self.labels.extend(sorted(patient.glob("*[0-9]_gt.nii.gz")))
+
         if num_training_cases is not None:
             self.patients = self.patients[:num_training_cases]
+            self.labels = self.labels[:num_training_cases]
 
         self.transform = transform
-        self.cardiac_phase_indexes = self._get_cardiac_phase_indexes()
 
     def _get_cardiac_phase_indexes(self):
         """Reads the CSV metadata file to extract the end diastole and end systole frames for each volume.
@@ -71,20 +77,15 @@ class MNMsDataset(Dataset):
         return len(self.patients)
 
     def __getitem__(self, index):
-        patient_dir = self.patients[index]
-        patient = patient_dir.name
+        image = nib.load(self.patients[index])
+        label = nib.load(self.labels[index])
+        patient = self.patients[index].parent.name
 
-        image = nib.load(patient_dir / f"{patient}_sa.nii.gz")
-        label = nib.load(patient_dir / f"{patient}_sa_gt.nii.gz")
-        image = image.get_fdata(dtype=np.float32)
-        label = label.get_fdata(dtype=np.float32)
-
-        randomized_phase = np.random.choice(["end_diastole", "end_systole"])
-
-        image = image[..., self.cardiac_phase_indexes[patient][randomized_phase]]
-        label = label[..., self.cardiac_phase_indexes[patient][randomized_phase]]
-
-        sample = {"image": image, "label": label, "patient": patient}
+        sample = {
+            "image": image.get_fdata(dtype=np.float32),
+            "label": label.get_fdata(dtype=np.float32),
+            "patient": patient,
+        }
 
         if self.transform:
             sample = self.transform(sample)
