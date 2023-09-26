3bfd625b64f54e01f3a6e9fa2c1f3836622bce3c
Josh <josh.e.stein@gmail.com>
2023-09-26 10:03:05 +0200

    Use 95th percentile for HD scores
    Match DeepMind implementation used in nnUNet pipeline

--- src/metrics.py
+++ src/metrics.py
@@ -8,8 +8,8 @@ METRICS = {
     "dice_with_background": DiceMetric(include_background=True, reduction="mean", num_classes=4),
     "dice_per_label": DiceMetric(include_background=False, reduction="mean_batch", num_classes=4),
     "dice_per_label_with_background": DiceMetric(include_background=True, reduction="mean_batch", num_classes=4),
-    "hausdorff": HausdorffDistanceMetric(include_background=False, reduction="mean"),
-    "hausdorff_per_label": HausdorffDistanceMetric(include_background=False, reduction="mean_batch"),
+    "hausdorff": HausdorffDistanceMetric(include_background=False, reduction="mean", percentile=0.95),
+    "hausdorff_per_label": HausdorffDistanceMetric(include_background=False, reduction="mean_batch", percentile=0.95),
 }
 
 
